f8ad4553b0f4a86a113d1b675b917bd2e33003b6
k-sokolov <ksokolov@uos.de>
2018-01-16 16:19:25 +0100

    modifying nav bar

--- globlnews/settings.py
+++ globlnews/settings.py
@@ -134,6 +134,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'globlnews/static/'),  # Here you tell django to look for a folder named 'assets'
+]
+
 
 TEMPLATE_DIRS = (
     os.path.join(BASE_DIR, 'globlnews/templates/'),
