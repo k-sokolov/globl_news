0c3937bcb980d077d0416c76d55d5f980f643a34
k-sokolov <ksokolov@uos.de>
2018-01-23 16:30:30 +0100

    modified search, back and front

--- summaries/forms.py
+++ summaries/forms.py
@@ -4,6 +4,8 @@ import datetime
 from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Layout, ButtonHolder, Submit
 
+from haystack.query import EmptySearchQuerySet, SearchQuerySet
+
 from . import models
 
 class SummaryForm(forms.ModelForm):
@@ -33,3 +35,51 @@ class SummaryForm(forms.ModelForm):
     #def clean_publication_date_original_article(self):
     #    publication_date = self.cleaned_data.get('publication_date_original_article')
     #    current_date = datetime.datetime.now()
+
+
+        
+class SearchForm(forms.Form):
+    q = forms.CharField(required=False, label=('Search'),
+                        widget=forms.TextInput(attrs={'type': 'search'}))
+
+    def __init__(self, *args, **kwargs):
+        self.searchqueryset = kwargs.pop('searchqueryset', None)
+        self.load_all = kwargs.pop('load_all', False)
+
+        if self.searchqueryset is None:
+            self.searchqueryset = SearchQuerySet()
+
+        super(SearchForm, self).__init__(*args, **kwargs)
+
+    def no_query_found(self):
+        """
+        Determines the behavior when no query was found.
+
+        By default, no results are returned (``EmptySearchQuerySet``).
+
+        Should you want to show all results, override this method in your
+        own ``SearchForm`` subclass and do ``return self.searchqueryset.all()``.
+        """
+        return EmptySearchQuerySet()
+
+    def search(self):
+        if not self.is_valid():
+            return self.no_query_found()
+
+        if not self.cleaned_data.get('q'):
+            return self.no_query_found()
+
+        sqs = self.searchqueryset.auto_query(self.cleaned_data['q'])
+
+        if self.load_all:
+            sqs = sqs.load_all()
+
+        return sqs
+
+    def get_suggestion(self):
+        if not self.is_valid():
+            return None
+
+        return self.searchqueryset.spelling_suggestion(self.cleaned_data['q'])
+
+
